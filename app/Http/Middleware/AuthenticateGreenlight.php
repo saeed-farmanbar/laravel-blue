1194259afe408b69ff27973945ac1d52598b3fa4
saeed farmanbar <saeed_farmanbar@yahoo.com>
2022-01-29 01:30:46 +0330

    debug

--- app/Http/Middleware/AuthenticateGreenlight.php
+++ app/Http/Middleware/AuthenticateGreenlight.php
@@ -20,25 +20,26 @@ class AuthenticateGreenlight
     {
 
         // AuthenticateGreenlight
-        $address = "http://meet.mohit.art/b/user-info-api";
+        $address = "https://meet.mohit.art/b/user-info-api";
         
         //your cookie
-        if(isset($_COOKIE['_greenlight-2_3_session'])){
+        // if(isset($_COOKIE['_greenlight-2_3_session'])){
 
-            $cookie = ['Cookie' => "_greenlight-2_3_session=".$_COOKIE['_greenlight-2_3_session']];
+            // $cookie = ['Cookie' => "_greenlight-2_3_session=".$_COOKIE['_greenlight-2_3_session']];
+            $cookie = ['Cookie' => "_greenlight-2_3_session=RE7xX%2F2v4Fezf6TjdhHGjh40ytwbiMgf%2F%2FWAbTFE%2BAUzg0nWvyPvGuDOcDEFmqz%2BDOD1sVZk3v0wbgFP5NAyPPg2i9q3vqFWTrlxpbLEOXsuNA%2FcDoXtAWBJn28wrzWBy1lZMzT2hNt%2BAYA1V0uJYgYlrpEQ7ldHdS8%3D--rYSPX7cqiXmt1oE1--k2MK3zqp5EOzBM6QiO%2F5gw%3D%3D"];
         
+            
             //your request
             $res = Http::withOptions([
                 'headers' => $cookie
             ])->get($address);
             
-            logger($_COOKIE['_greenlight-2_3_session']);
-            logger("res:".$res->json());
-            logger("res:".$res->body());
+            // logger($_COOKIE['_greenlight-2_3_session']);
+            // logger("res:".$res->json());
+            // logger("res:".$res->body());
             logger("res:".$res);
             logger("condition:");
             logger(empty($res));
-
 
 
             app()->bind('user', function ($app) use ($res){
@@ -49,9 +50,12 @@ class AuthenticateGreenlight
                 return $res!=null?true:false;
             });
             // logger(app("authenticated"));
-            return $next($request);
-        }
+            die();
 
+            return $next($request);
+        // }
+
+        die();
 
         
         app()->bind('authenticated', function ($app) {
