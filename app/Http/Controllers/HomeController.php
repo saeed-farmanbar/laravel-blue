066969582b0aec79c8587f6dd32afbf9be19a131
saeed farmanbar <saeed_farmanbar@yahoo.com>
2022-01-30 00:31:37 +0330

    fix some data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -44,7 +44,7 @@ class HomeController extends Controller
             return back();
         }
 
-        $user=\DB::table('users')->where("uid"app("user")["uid"])->firstOrFail();
+        $user=\DB::table('users')->where("uid",app("user")["uid"])->firstOrFail();
         $user->language=$lang;
         $user->save();
         return back();
