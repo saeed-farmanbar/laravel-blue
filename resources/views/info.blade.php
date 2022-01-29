38ed1684f7c6c704b405744126ad4f1b50b5cdd5
saeed farmanbar <saeed_farmanbar@yahoo.com>
2022-01-30 00:17:13 +0330

    fix some data

--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -222,12 +222,12 @@ Contact us: contact@mohit.art
 <br>
 <a href="http://mohit.art">mohit.art</a>
 
-
+<a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Flag_of_Iran.svg/23px-Flag_of_Iran.svg.png"></a>
+<a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Flag_of_Iran.svg/23px-Flag_of_Iran.svg.png"></a>
+<a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Flag_of_Iran.svg/23px-Flag_of_Iran.svg.png"></a>
         </p>
         @if (app("authenticated") )
-        <a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Flag_of_Iran.svg/23px-Flag_of_Iran.svg.png"></a>
-        <a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Flag_of_Iran.svg/23px-Flag_of_Iran.svg.png"></a>
-        <a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Flag_of_Iran.svg/23px-Flag_of_Iran.svg.png"></a>
+ss
         @endif
 
 
