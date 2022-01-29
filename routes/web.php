f8b2751e61e5bc1ec0e3d14a4afba6de13e34beb
saeed farmanbar <saeed_farmanbar@yahoo.com>
2022-01-29 22:19:10 +0330

    fix some data

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\HomeController;
 |
 */
 Route::get('/', [HomeController::class, 'show']);
-Route::get('/b/logout', [HomeController::class, 'logout']);
+Route::get('/u/logout', [HomeController::class, 'logout']);
 
 // Route::get('/', function () {
 //     return view('info');
