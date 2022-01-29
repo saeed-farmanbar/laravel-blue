350e2e4449971950c1ccc0a0477b259f41fa5430
saeed farmanbar <saeed_farmanbar@yahoo.com>
2022-01-29 22:32:11 +0330

    fix some data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,9 +19,18 @@ class HomeController extends Controller
 
     public function logout()
     {
-        logger("sssssssssss");
-        unset($_COOKIE['_greenlight-2_3_session']);
-        setcookie("_greenlight-2_3_session", "", -1);
+        if (isset($_SERVER['HTTP_COOKIE'])) {
+            $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
+            foreach($cookies as $cookie) {
+                $parts = explode('=', $cookie);
+                $name = trim($parts[0]);
+                setcookie($name, '', time()-1000);
+                setcookie($name, '', time()-1000, '/');
+            }
+        }
+        
+
+
         return back();
     }
 
